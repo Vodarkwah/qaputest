3eb364381f882a0c87431f2123c50e642a962154
Victor Owusu Darkwah <92731623+Vodarkwah@users.noreply.github.com>
2022-02-07 14:53:33 +0000

    testrun

---
+++ qapu.js
@@ -0,0 +1,25 @@
+
+
+function SelectRedirect() {
+switch (document.getElementById('colleges').value) {
+    case "COE":
+        window.location = "./COE.html";
+        break;
+
+    case "COS":
+        window.location = "./COS.html";
+        break;
+
+    case "CANR":
+        window.location = "./CANR.html";
+        break;
+    case "COHSS":
+        window.location = "./COHSS.html";
+        break;
+
+    case "FOSS":
+        window.location = "./FOSS.html";
+        break;
+    default:
+        window.location = "./qapu.html"; // if no selection matches then redirected to home page
+        break;}}
