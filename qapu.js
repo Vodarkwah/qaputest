bff48baff37a83f5e81fe319546029da00e9c4f2
Victor Owusu Darkwah <92731623+Vodarkwah@users.noreply.github.com>
2022-03-31 18:45:36 +0000

    Add files via upload
    testrun

---
+++ qapu.js
@@ -0,0 +1,34 @@
+
+function SelectRedirect() {//function to loop through all the values in colleges
+
+switch (document.getElementById('colleges').value) {
+   
+    case "COE":
+      //  console.log(e);
+        window.location = "./COE.html";
+        break;
+
+    case "COS":
+        window.location = "./COS.html";
+        break;
+
+    case "CANR":
+        window.location = "./CANR.html";
+        break;
+    case "COHSS":
+        window.location = "./COHSS.html";
+        break;
+
+    case "FOSS":
+        window.location = "./FOSS.html";
+        break;
+    default:
+        window.location = "./qapu.html"; // if no selection matches then redirected to home page
+        break;}}
+
+var submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+    event.preventDefault();
+    SelectRedirect();
+
+});
